c27c3462f32e992936d030beec15f58ce91d52af
aleoliu566 <aleoliu566@gmail.com>
2020-09-29 01:45:41 +0800

    Updates

---
+++ precache-manifest.ecb3d131837b551405184ab341078cda.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d23035df62515340c03b5426e1172c5a",
+    "url": "/TSMC-CHATBOT/index.html"
+  },
+  {
+    "revision": "0268e810adf74f0edbfc",
+    "url": "/TSMC-CHATBOT/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "cf3f87c81472c040e0e8",
+    "url": "/TSMC-CHATBOT/static/js/2.2926902b.chunk.js"
+  },
+  {
+    "revision": "27bbadeef7d02efcaae5fffc16c6b291",
+    "url": "/TSMC-CHATBOT/static/js/2.2926902b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0268e810adf74f0edbfc",
+    "url": "/TSMC-CHATBOT/static/js/main.286b16f0.chunk.js"
+  },
+  {
+    "revision": "a6563e9ebc791db11c67",
+    "url": "/TSMC-CHATBOT/static/js/runtime-main.c619b5f7.js"
+  }
+]);
