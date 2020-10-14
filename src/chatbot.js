24b366010080c9655b26a21cf884f7c945d35f68
aleoliu566 <aleoliu566@gmail.com>
2020-10-15 00:04:11 +0800

    [Feature] Add Chatbot Notification in Page Title

--- src/chatbot.js
+++ src/chatbot.js
@@ -2,31 +2,48 @@ import React from 'react';
 import './App.css';
 import firebase from 'firebase';
 import config from './config';
+import { Helmet } from 'react-helmet'
 
 class Chatbot extends React.Component{
   constructor(props) {
     super(props);
 
     this.state={
+      chatBotNumber: 25,
       chatContent: {},
       value: '',
       userName: '',
+      pageTitle: 'LEEMI',
+      lastTimeFocus: '',
     }
 
     this.handleChangeName = this.handleChangeName.bind(this);
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleKeyDown = this.handleKeyDown.bind(this);
+    this.handleFocus = this.handleFocus.bind(this);
   }
 
   componentDidMount() {
+    console.log(Date.now());
     let chatbotLists = [];
     if (!firebase.apps.length) {
       this.firebaseApp = firebase.initializeApp(config);
       this.database = this.firebaseApp.database();
 
-      this.database.ref('/Test').limitToLast(20).on("value", e => {
-        this.setState({ chatContent: e.val() });
+      this.database.ref('/Test').limitToLast(this.state.chatBotNumber).on("value", e => {
+        this.setState({
+          chatContent: e.val(),
+          lastTimeFocus: e.val()[Object.keys(e.val())[this.state.chatBotNumber-1]].timestamp
+        },function(){
+          let lastTimeRead = localStorage.getItem("lastTimeFocus");
+          console.log("egfegwww");
+
+          if(lastTimeRead < this.state.lastTimeFocus){
+            console.log("egfeg");
+            this.setState({pageTitle: '(*) notification '})
+          }  
+        });
       });
     }
   }
@@ -52,8 +69,13 @@ class Chatbot extends React.Component{
     }
   }
 
+  handleFocus(event) {
+    localStorage.setItem('lastTimeFocus', this.state.lastTimeFocus);
+    this.setState({pageTitle: 'LEEMI'})
+  }
+
   sendMessege() {
-    let messege = this.state.userName + ": " + this.state.value;
+    let timestampNow = Date.now();
     var dt = new Date();
     this.firebaseApp.database().ref('/Test').push({
       content:this.state.userName + ': ' + this.state.value,
@@ -63,13 +85,14 @@ class Chatbot extends React.Component{
           + dt.getHours() + ':'
           + dt.getMinutes() + ':'
           + dt.getSeconds(),
+      timestamp: timestampNow,
       user: this.state.userName
     });
+    localStorage.setItem('lastTimeFocus', timestampNow);
     this.setState({value: ''});
   }
 
   isUser(contentUser) {
-    console.log(contentUser);
     if(contentUser === this.state.userName) {
       return 'contentRight';
     } else {
@@ -89,12 +112,19 @@ class Chatbot extends React.Component{
 
     return (
       <div className="App">
+        <Helmet>
+          <title>{this.state.pageTitle}</title>
+        </Helmet>
         <div className="chatContentBox">
           {chatbot}
         </div>      
         <div className="chatSubmitBox">
           <input type="text" value={this.state.userName} onChange={this.handleChangeName} />
-          <input type="text" value={this.state.value} onChange={this.handleChange}  onKeyPress={this.handleKeyDown}/>
+          <input type="text" value={this.state.value}
+            onChange={this.handleChange}
+            onKeyPress={this.handleKeyDown}
+            onFocus={this.handleFocus}
+          />
           <input type="submit" value="Submit" onClick={this.handleSubmit}/>
         </div>
       </div>
